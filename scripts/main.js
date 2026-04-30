d0efef84c8f0cf41c49d5fd356d312d0be927470
cookscamuel <cookadamsamuel@gmail.com>
2026-04-30 17:08:24 -0300

    Revised structure, added temporary assets, polished overall functionality.

--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,79 @@
-import treeQueue from "./tree.js";
+import treeQueue from './tree.js';
 
+const IDLE = loadAsset('./media/textures/idle.png');
+const CUT = loadAsset('./media/textures/cut.png');
+const DEAD = loadAsset('./media/textures/dead.png');
+IDLE.id = CUT.id = DEAD.id ='player';
+
+const BRANCH = loadAsset('./media/textures/branch.svg');
+
+document.addEventListener('click', startGame);
 // TODO:
+// Add comments.
+// Revise readme.
+// Reorganize function order.
 // Replace onClicks with ontouchstart, apparently this will work with mobile devices to provide instant effects.
-// Use a queue for the tree. Add new logs to the back, remove ones from the front.
-// Make it so the bottom of the tree always begins as being a log with no branches.
 // Use window.innerHeight and innerWidth to size the segments according to the screen size.
 
-document.addEventListener("click", begin);
+
+// Make lumberjack-idle and lumberjack-cut gifs.
+// Only need one branch texture, just flip it.
+
+// A function to help load textures in memory ahead of time.
+function loadAsset(url) {
+    const img = new Image();
+    img.src = url;
+    return img;
+}
+
+function addNewTreeSegment(rear, score, i) {
+    var segment = rear == 0 || rear == 2 ?
+        document.createElement('img') :
+        document.createElement('div');
+
+    segment.id = i == -1 ? 'log-' + (score + 6) : 'log-' + i;
+
+    segment.className = 'log-segments';
+
+    if (rear == 1) return segment;
+
+    segment.src = BRANCH.src;
+    segment.style.translate = rear == 0 ? '-70%' : '5%';
+    segment.style.transform = rear == 0 ? 'scaleX(1)' : 'scaleX(-1)';
+
+    return segment;
+}
+
 
 // Function used to setup the game.
-function begin() {
+function startGame() {
 
     // Prevent multiple clicks and remove the start screen.
-    document.removeEventListener("click", begin);
+    document.removeEventListener('click', startGame);
 
     // Remove the loading prompt.
     document.getElementById('click-prompt').remove()
-    
+
     // Create the tree queue.
     const theTree = new treeQueue();
 
     // Get the left, center, and right parts of the screen.
     var screenLeft = document.getElementById('screen-left');
-    var screenCenter = document.getElementById("screen-center");
+    var screenCenter = document.getElementById('screen-center');
     var screenRight = document.getElementById('screen-right');
-    var body = document.getElementById("body");
-    
+    var body = document.getElementById('body');
+
     buildTree(theTree, screenCenter);
-    
-    var player = document.createElement("img");
-    player.src = "./media/lumberjack-0.png";
-    player.style.width = "20vh";
-    player.style.display = "block";
-    player.style.margin = "auto";
+
+    var player = new Image();
+    player.id = 'player';
+    player = IDLE;
+
     screenLeft.appendChild(player);
-    
-    screenLeft.addEventListener("click", () => chop(theTree, screenLeft, screenCenter, screenRight, player, body));
-    screenRight.addEventListener("click", () => chop(theTree, screenLeft, screenCenter, screenRight, player, body));
+
+    for (var side of [screenLeft, screenRight]) {
+        side.addEventListener('click', () => chop(theTree, screenLeft, screenCenter, screenRight, player, body));
+    }
 
     handlePlayerSide(theTree, screenLeft, screenRight, player, 0);
 
@@ -47,37 +84,18 @@ function begin() {
 
 function buildTree(theTree, screenCenter) {
     for (var i = 0; i < 6; i++) {
-        var newLog = document.createElement("p");        
+        var newLog = document.createElement('p');
         i % 2 == 0 ? theTree.addLog(1) : theTree.addLog(Math.floor(Math.random() * 3));
 
-        newLog.innerText = "LOG" + theTree.getRear();
-        newLog.id = "log-" + i;
+        newLog.innerText = 'LOG' + theTree.getRear();
+        newLog.id = 'log-' + i;
 
-        var newSegment = document.createElement("img");
+        var newSegment = addNewTreeSegment(theTree.getRear(), theTree.getScore(), i);
 
-        switch (theTree.getRear()) {
-            case 0:
-                newSegment.src = "./media/goldenFrasier.jpg";
-                break;
-            case 1:
-                newSegment.src = "./media/sleepyHollow.PNG";
-                break;
-            case 2:
-                newSegment.src = "./media/frasier.jpg";
-                break;
-        }
-
-        newSegment.style.width = "50vw";
-        newSegment.style.height = "50px";
-        newSegment.style.position = "relative";
-        newSegment.style.pointerEvents = "none";
-        newSegment.id = "log-" + i;
-        newSegment.className = "log-segments";
-        
-        i == 0 ? screenCenter.appendChild(newSegment) : screenCenter.insertBefore(newSegment, document.getElementById("log-" + (i - 1)));
+        i == 0 ? screenCenter.appendChild(newSegment) : screenCenter.insertBefore(newSegment, document.getElementById('log-' + (i - 1)));
     }
 
-    document.getElementById("score").innerText = "0";
+    document.getElementById('score').innerText = '0';
 
 }
 
@@ -87,113 +105,128 @@ function handlePlayerSide(theTree, screenLeft, screenRight, player, side) {
     switch (side) {
         // Place player on the left.
         case 0:
-            screenRight.innerHTML = '';
+            screenLeft.innerHTML = screenRight.innerHTML = '';
+            player = CUT;
+            player.style.transform = 'scaleX(1)';
             screenLeft.appendChild(player);
+            setTimeout(function(){
+                player = IDLE;
+                screenLeft.innerHTML = '';
+                player.style.transform = 'scaleX(1)';
+                screenLeft.appendChild(player);
+            }, 100);
             break;
-        
+
         // Place player on the right.
         case 1:
-            screenLeft.innerHTML = '';
+            screenLeft.innerHTML = screenRight.innerHTML = '';
+            player = CUT;
+            player.style.transform = 'scaleX(-1)';
             screenRight.appendChild(player);
+            setTimeout(function(){
+                player = IDLE;
+                screenRight.innerHTML = '';
+                player.style.transform = 'scaleX(-1)';
+                screenRight.appendChild(player);
+            }, 100);
             break;
     }
+
 
 }
 
 function chop(theTree, screenLeft, screenCenter, screenRight, player, body) {
 
+    var clickedSide = event.target.id;
     // Flip sides.
-    event.target.id == "screen-left" ? handlePlayerSide(theTree, screenLeft, screenRight, player, 0) : handlePlayerSide(theTree, screenLeft, screenRight, player, 1);
+    clickedSide == 'screen-left' ? handlePlayerSide(theTree, screenLeft, screenRight, player, 0) : handlePlayerSide(theTree, screenLeft, screenRight, player, 1);
 
-    // Check if they are going into a branch at the ground level.
-    if ((event.target.id == "screen-left" && theTree.getFront() == 0) || (event.target.id == "screen-right" && theTree.getFront() == 2)) {
-        gameOver(theTree);
-        return;
-    }
-    
-    theTree.removeLog();
-    document.getElementById("log-" + theTree.getScore()).remove();
+    setTimeout(function() {
 
-    // Make sure there is never a die-die situation where cutting either side kills you.
-    (theTree.getScore() + 6) % 2 == 0 ? theTree.addLog(1) : theTree.addLog(Math.floor(Math.random() * 3));
-
-        var newSegment = document.createElement("img");
-
-        switch (theTree.getRear()) {
-            case 0:
-                newSegment.src = "./media/goldenFrasier.jpg";
-                break;
-            case 1:
-                newSegment.src = "./media/sleepyHollow.PNG";
-                break;
-            case 2:
-                newSegment.src = "./media/frasier.jpg";
-                break;
+        // Check if they are going into a branch at the ground level.
+        if ((clickedSide == 'screen-left' && theTree.getFront() == 0) || (clickedSide == 'screen-right' && theTree.getFront() == 2)) {
+            
+            gameOver(player, screenLeft, screenRight, theTree.getScore());
+            return;
+        }
+        
+        theTree.removeLog();
+        document.getElementById('log-' + theTree.getScore()).remove();
+        
+        // Make sure there is never a die-die situation where cutting either side kills you.
+        (theTree.getScore() + 6) % 2 == 0 ? theTree.addLog(1) : theTree.addLog(Math.floor(Math.random() * 3));
+        
+        var newSegment = addNewTreeSegment(theTree.getRear(), theTree.getScore(), -1);
+        
+        
+        screenCenter.insertBefore(newSegment, document.getElementById('log-' + (theTree.getScore() + 5)));
+        
+        // Check if a branch has fallen on top of the player.
+        if ((clickedSide == 'screen-left' && theTree.getFront() == 0) || (clickedSide == 'screen-right' && theTree.getFront() == 2)) {
+            gameOver(player, screenLeft, screenRight, theTree.getScore());
+            return;
         }
 
-        newSegment.style.width = "50vw";
-        newSegment.style.height = "50px";
-        newSegment.style.position = "relative";
-        newSegment.style.pointerEvents = "none";
-        newSegment.id = "log-" + (theTree.getScore() + 6);
-        newSegment.className = "log-segments";
-        
-    screenCenter.insertBefore(newSegment, document.getElementById("log-" + (theTree.getScore() + 5)));
-
-    // Check if a branch has fallen on top of the player.
-    if ((event.target.id == "screen-left" && theTree.getFront() == 0) || (event.target.id == "screen-right" && theTree.getFront() == 2)) {
-        gameOver(theTree);
-        return;
-    }
-    
-    // Increase and update the score.
-    theTree.score++;
-    document.getElementById("score").innerText = theTree.getScore();
+        // Increase and update the score.
+        theTree.score++;
+        document.getElementById('score').innerText = theTree.getScore();
+    }, 100);
 
 }
 
 // Function called when the player loses.
-function gameOver(theTree) {
-    body.innerHTML = '';
-    body.style.background = 'linear-gradient(#9e2424, #701212)';
-    body.style.display = 'block';
+function gameOver(player, left, right, score) {
+
+        var diedSide = player.parentElement;
+        left.innerHTML = right.innerHTML = '';
+        player = DEAD;
+        diedSide.appendChild(player);
+
+    left.style.pointerEvents = right.style.pointerEvents = 'none';
+
+    setTimeout(function() {
+
+        body.innerHTML = '';
+        body.style.background = 'linear-gradient(#9e2424, #701212)';
+        body.style.display = 'block';
+        
+        var gameOverTitle = document.createElement('p');
+        gameOverTitle.id = 'game-over';
+        gameOverTitle.innerText = 'GAME OVER!\nSCORE: ' + score;
+        body.appendChild(gameOverTitle);
+        
+        var restartButton = document.createElement('button');
+        restartButton.id = 'restart-button';
+        restartButton.innerText = 'RETRY';
+        body.appendChild(restartButton);
+        restartButton.addEventListener('click', restart);
+
+    }, 1000);
     
-    var gameOverTitle = document.createElement("p");
-    gameOverTitle.id = "game-over";
-    gameOverTitle.innerText = "GAME OVER!\nSCORE: " + theTree.getScore();
-    body.appendChild(gameOverTitle);
-    
-    var restartButton = document.createElement("button");
-    restartButton.id = "restart-button";
-    restartButton.innerText = "RETRY";
-    body.appendChild(restartButton);
-    restartButton.addEventListener("click", restart);
 }
-
-
 
 // Function used to restart the game.
 function restart() {
     body.innerHTML = '';
     body.style = '';
-    
-    var replaceLeft = document.createElement("div");
-    var replaceCenter = document.createElement("div");
-    var replaceRight = document.createElement("div");
-    var replaceScore = document.createElement("div");
-    var replacePrompt = document.createElement("div");
 
-    replaceLeft.id = "screen-left";
-    replaceCenter.id = "screen-center";
-    replaceRight.id = "screen-right";
-    replaceScore.id = "score";
-    replacePrompt.id = "click-prompt";
+    var replaceLeft = document.createElement('div');
+    var replaceCenter = document.createElement('div');
+    var replaceRight = document.createElement('div');
+    var replaceScore = document.createElement('div');
+    var replacePrompt = document.createElement('div');
+
+    replaceLeft.id = 'screen-left';
+    replaceCenter.id = 'screen-center';
+    replaceRight.id = 'screen-right';
+    replaceScore.id = 'score';
+    replacePrompt.id = 'click-prompt';
 
     body.appendChild(replaceLeft);
     body.appendChild(replaceCenter);
     body.appendChild(replaceRight);
-    replaceCenter.appendChild(replaceScore);
+    body.appendChild(replaceScore);
     replaceCenter.appendChild(replacePrompt);
 
-    begin();
+    startGame();
 }
